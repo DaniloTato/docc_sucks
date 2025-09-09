3690706f5f9794a9492e8af6f9661f2e6f5726e5
DaniloTato <Noentiendo2005@gmail.com>
2025-09-08 22:17:10 -0600

    fixed is_substring function incorrect renaming bug

--- code/src/is_substr.cpp
+++ code/src/is_substr.cpp
@@ -1,8 +1,8 @@
 #include "checks.hpp"
 
 int is_substr(const string &seq, const string &sub) {
-    int n = seq.size();
-    int m = sub.size();
+    int n = sub.size();
+    int m = seq.size();
 
     if (n == 0 || m == 0)
         return -1;
@@ -11,17 +11,17 @@ int is_substr(const string &seq, const string &sub) {
     vector<range> answer;
 
     for (int i = 0; i < n; i++) {
-        badchar[(int)seq[i]] = i;
+        badchar[(int)sub[i]] = i;
     }
 
     int s = 0;
     while (s + n < m) {
         int j = n - 1;
 
-        while (j >= 0 && seq[j] == sub[s + j])
+        while (j >= 0 && sub[j] == seq[s + j])
             j--;
 
-        char next_char_after_pattern = sub[s + n];
+        char next_char_after_pattern = seq[s + n];
         int next_char_value = badchar[next_char_after_pattern];
         if (j < 0) {
             answer.push_back(std::make_pair(s, s + n - 1));
